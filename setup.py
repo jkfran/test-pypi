4c87e1ccee966c325f978327f8f01763a963d00f
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2019-11-18 13:30:15 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="hello-jkfran",
+    version="0.1",
+    scripts=["hello-jkfran"],
+    author="Francisco Jimenez Cabrera",
+    author_email="jkfran@gmail.com",
+    description="Hello jkfran",
+    long_description="Hello jkfran",
+    long_description_content_type="text/plain",
+    url="https://github.com/jkfran/test-pypi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
